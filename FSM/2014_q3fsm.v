c5b9cee79c227cb55a7bb644dc1d11e50934123a
ziyiguo <123039540+VientoGuo@users.noreply.github.com>
2024-02-07 18:39:26 -0500

    Update 2014_q3fsm.v

--- FSM/2014_q3fsm.v
+++ FSM/2014_q3fsm.v
@@ -56,3 +56,43 @@ module top_module (
         end
     end
 endmodule
+
+
+// solution 2
+// debug code: when the current state output depends on previous output, should use @(posedge clk) to write output
+module top_module (
+    input clk,
+    input reset,   // Synchronous reset
+    input s,
+    input w,
+    output z
+);
+    parameter A=0,B=1,BIT2=2,BIT3=3;
+    reg [1:0]count_ones;
+    reg [1:0] state, next_state;
+    always@(posedge clk)begin
+        if(reset) state = A;
+        else state = next_state;
+    end
+    always@(*)begin
+        case(state)
+            A: next_state = s?B:A;
+            B: next_state = BIT2;
+            BIT2:next_state = BIT3;
+            BIT3:next_state = B;
+        endcase
+    end
+    always@(posedge clk)begin
+        if(reset) begin
+            z= 0;
+            count_ones = 0;
+        end else begin
+        if((state== BIT2)|(state==BIT3)) count_ones += w;
+        z = (state==BIT3)&(count_ones==2);
+        if(state == B) count_ones =w?1:0;
+        end
+    end
+    
+
+endmodule
+
